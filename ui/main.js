9c38fb7cf17811ae83b85b7ee8869b7168dbd5ac
Muhammed Shasin P <mohdshasin313@gmail.com>
2017-06-03 22:04:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,8 @@
 (function() {
 
 function clock() {
-    document.getElementById('clock-div').innerHtml = Date.now();
+    var d = new Date();
+    document.getElementById('clock-div').innerHtml = d.toLocaleTimeString();
 }
 
 setInterval(clock, 1000);
