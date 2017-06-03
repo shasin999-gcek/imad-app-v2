f0f8ad4847f27bd0eccd650f181199090aaf698a
Muhammed Shasin P <mohdshasin313@gmail.com>
2017-06-03 21:41:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,6 @@
-console.log('Loaded!');
+var clock = function() {
+    var d = new Date();
+    document.getElementById('clock-div').innerHtml = d.toLocalTimeString();
+}
+
+setInterwell(clock, 1000);
